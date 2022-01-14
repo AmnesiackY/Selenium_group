384fd063e6d3a2947fa69670a763e70d7924282d
AmnesiackY <hitsac2711@gmail.com>
2022-01-14 23:38:48 +0200

    start work on Footer by Dimas

---
+++ Test_UI_Practice/Test_UI_Practice/PageObjects/Footer/Footer.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+//Делал Димас
+namespace Test_UI_Practice.tests
+{
+    public class Footer
+    {
+        protected IWebDriver _driver;
+    }
+}
---
+++ Test_UI_Practice/Test_UI_Practice/PageObjects/Footer/Footer_Women.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+//Делал Димас
+namespace Test_UI_Practice.tests
+{
+    public class Footer_Women : Footer
+    {
+        public Footer_Women(IWebDriver driver) 
+        {
+            _driver = driver;
+        }
+    }
+}
